7e604e9e814508530500eb22607b2d5920c27703
databord <hagovdatabord@gmail.com>
2025-12-18 00:16:05 -0300

    Agrega frases ro

--- POMPLA/frases.js
+++ POMPLA/frases.js
@@ -1,14 +1,3 @@
 const FRASES_MOTIVACIONALES = [
-    "El único modo de hacer un gran trabajo es amar lo que haces. - Steve Jobs",
-    "No cuentes los días, haz que los días cuenten. - Muhammad Ali",
-    "El éxito es la suma de pequeños esfuerzos repetidos día tras día. - Robert Collier",
-    "La disciplina es el puente entre metas y logros. - Jim Rohn",
-    "No esperes. El tiempo nunca será el justo. - Napoleon Hill",
-    "La acción es la clave fundamental de todo éxito. - Pablo Picasso",
-    "Tus sueños no tienen fecha de caducidad. Respira hondo y sigue. - Anónimo",
-    "Cree que puedes y casi lo habrás logrado. - Theodore Roosevelt",
-    "El futuro depende de lo que hagas hoy. - Mahatma Gandhi",
-    "La motivación es lo que te pone en marcha. El hábito es lo que hace que sigas. - Jim Ryun",
-    "No te detengas hasta que estés orgulloso. - Anónimo",
-    "Cada día es una nueva oportunidad para cambiar tu vida. - Anónimo"
+    "Es muy fácil amarte incluso en los días de caos - Ro",
 ];
